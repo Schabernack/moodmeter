2391243dd073cd127f2442806b7e564e2b68e543
nico <mister.wunderlich@gmail.com>
2011-11-23 18:06:46 +0100

    refactoring

--- mmeter.py
+++ mmeter.py
@@ -1,65 +1,66 @@
 import Image;
 import cv;
  
-class Mmeter:
 
-	#returns new, opened image
-	def morph_open(image, it=1):
-		img=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-		cv.Dilate(image, img, iterations=it)		
-		cv.Erode(img, img, iterations=it)
-		img
-		
-	#returns new, closed image
-	def morph_close(image, it=1):
-		img=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-		cv.Erode(img, img, iterations=it)
-		cv.Dilate(image, img, iterations=it)		
-		img
+
+class Mmeter:
 	
 	def __init__(self):
-		run()
-	
-	def run():
 		img = cv.LoadImage("img/1.jpg")
-		img_hsv= cv.CreateMat(img.height, img.width, cv.CV_8UC3)
-		cv.CvtColor(img, img_hsv, cv.CV_BGR2HSV)
-		img_tmp = cv.CreateMat(img.height, img.width, cv.CV_8UC3)
-		img_thresh = cv.CreateMat(img.height, img.width, cv.CV_8UC3)
+		
+		openimg=self.fg_disc(img)
 
-		h_plane=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-		s_plane=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-		v_plane=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
+		cv.ShowImage("opened img", openimg)
+		cv.MoveWindow("opened img", 20,20)
+
+		while cv.WaitKey(10)!=27:
+			x=1	
+			
+	
+	#returns new, opened image
+	def morph_open(self, image, it=1, kernel=None):	
+		img=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
+		cv.Erode(img, img, kernel, it)
+		cv.Dilate(image, img, kernel , it)		
+		return img
+		
+	#returns new, closed image
+	def morph_close(self, image, it=1, kernel=None):
+		img=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
+		cv.Dilate(image, img, kernel, it)		
+		cv.Erode(img, img, kernel,  it)
+		return img
+	
+	#foreground/background discrimination		
+	def fg_disc(self, image):
+		img_hsv= cv.CreateMat(image.height, image.width, cv.CV_8UC3)
+		cv.CvtColor(image, img_hsv, cv.CV_BGR2HSV)
+		img_tmp = cv.CreateMat(image.height, image.width, cv.CV_8UC3)
+		img_thresh = cv.CreateMat(image.height, image.width, cv.CV_8UC3)
+
+		h_plane=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
+		s_plane=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
+		v_plane=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
 		cv.Split(img_hsv, h_plane, s_plane, v_plane, None)
 
 		cv.Threshold(h_plane, h_plane, 18.0, 255.0 , cv.CV_THRESH_BINARY_INV)
 		cv.Threshold(s_plane, s_plane, 50.0, 255.0 , cv.CV_THRESH_BINARY)
 		cv.Threshold(v_plane, v_plane, 80.0, 255.0 , cv.CV_THRESH_BINARY)
-		
-		img_and=cv.CreateMat(img.height, img.width, cv.CV_8UC1)
+	
+		img_and=cv.CreateMat(image.height, image.width, cv.CV_8UC1)
 		cv.And(h_plane, s_plane, img_and)
 		cv.And(img_and, v_plane, img_and)
+	
+	
+		kernel5=cv.CreateStructuringElementEx(2,1,1,0, cv.CV_SHAPE_RECT)
+		kernell=cv.CreateStructuringElementEx(5,5,2,2, cv.CV_SHAPE_RECT)
 		
+		cv.Erode(img_and, img_and, iterations=2)	
+		cv.Erode(img_and, img_and, iterations=1, element=kernel5)	
+		openimg=self.morph_close(img_and, 30, kernel=kernell)
 		
-		openimg=morph_open(img_and)		
-		closeimg=morph_close(img_and)
-		closeopenimg=morph_close(openimg)
-
-
-		#cv.ShowImage("win1", img)
-		#cv.ShowImage("hue", h_plane)
-		#cv.ShowImage("saturation", s_plane)
-		#cv.ShowImage("value", v_plane)
-		#cv.ShowImage("win4", img_thresh)
-		
-		cv.ShowImage("opened img", openimg)
-		cv.ShowImage("closed image", closeimg)
-		cv.ShowImage("opened then closed", closeopenimg)
-
-		
-
-		while cv.WaitKey(10)!=27:
-			x=1	
+		return openimg
+	
 
 	
 	
